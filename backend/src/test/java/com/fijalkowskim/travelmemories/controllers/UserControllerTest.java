d6a3ae60da0d4fd1f147703f82a4a4f7952eb15a
Mateusz <fijalkowskim01@gmail.com>
2024-04-01 19:43:01 +0200

    UserController all changePassword tests

--- backend/src/test/java/com/fijalkowskim/travelmemories/controllers/UserControllerTest.java
+++ backend/src/test/java/com/fijalkowskim/travelmemories/controllers/UserControllerTest.java
@@ -1,16 +1,19 @@
 package com.fijalkowskim.travelmemories.controllers;
 
+import com.fijalkowskim.travelmemories.exceptions.CustomHTTPException;
 import com.fijalkowskim.travelmemories.services.UserService;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -43,5 +46,50 @@ public class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON);
         this.mvc.perform(request)
                 .andExpect(status().isOk());
+    }
+    @Test
+    public void testChangePassword_SamePasswords() throws Exception{
+        String email = "user1@email.com";
+        String oldPassword = "oldPassword";
+        String newPassword = "oldPassword";
+        Mockito.doThrow(new CustomHTTPException("Passwords must differ", HttpStatus.BAD_REQUEST))
+                .when(userService).changePassword(email,oldPassword,newPassword);
+        RequestBuilder request  = MockMvcRequestBuilders.put("/api/user/changePassword")
+                .param("email", email)
+                .param("oldPassword", oldPassword)
+                .param("newPassword", newPassword)
+                .contentType(MediaType.APPLICATION_JSON);
+        this.mvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+    @Test
+    public void testChangePassword_WrongPassword() throws Exception{
+        String email = "user1@email.com";
+        String oldPassword = "wrongPassword";
+        String newPassword = "newPassword";
+        Mockito.doThrow(new CustomHTTPException("Wrong password", HttpStatus.BAD_REQUEST))
+                .when(userService).changePassword(email,oldPassword,newPassword);
+        RequestBuilder request  = MockMvcRequestBuilders.put("/api/user/changePassword")
+                .param("email", email)
+                .param("oldPassword", oldPassword)
+                .param("newPassword", newPassword)
+                .contentType(MediaType.APPLICATION_JSON);
+        this.mvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+    @Test
+    public void testChangePassword_NoSuchUser() throws Exception{
+        String email = "baduser@email.com";
+        String oldPassword = "oldPassword";
+        String newPassword = "newPassword";
+        Mockito.doThrow(new CustomHTTPException("No user with such email", HttpStatus.BAD_REQUEST))
+                .when(userService).changePassword(email,oldPassword,newPassword);
+        RequestBuilder request  = MockMvcRequestBuilders.put("/api/user/changePassword")
+                .param("email", email)
+                .param("oldPassword", oldPassword)
+                .param("newPassword", newPassword)
+                .contentType(MediaType.APPLICATION_JSON);
+        this.mvc.perform(request)
+                .andExpect(status().isBadRequest());
     }
 }
