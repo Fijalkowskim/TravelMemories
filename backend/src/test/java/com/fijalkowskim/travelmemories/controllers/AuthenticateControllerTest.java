98ff8fafc8277d14d5208662202ad55d776bb0cd
Mateusz <fijalkowskim01@gmail.com>
2024-04-01 19:54:04 +0200

    AuthenticateController tests

---
+++ backend/src/test/java/com/fijalkowskim/travelmemories/controllers/AuthenticateControllerTest.java
@@ -0,0 +1,67 @@
+package com.fijalkowskim.travelmemories.controllers;
+
+import com.fijalkowskim.travelmemories.exceptions.CustomHTTPException;
+import com.fijalkowskim.travelmemories.responsemodels.AuthenticateResponse;
+import com.fijalkowskim.travelmemories.services.AuthenticateService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class AuthenticateControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AuthenticateService authenticateService;
+    @Mock
+    private AuthenticateResponse authenticateResponse;
+
+    @Test
+    public void testLogin_Successfully() throws Exception {
+        String email = "user1@email.com";
+        String password = "password";
+
+        Mockito.when(authenticateService.login(email, password)).thenReturn(authenticateResponse);
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/api/public/authenticate/login")
+                .param("email", email)
+                .param("password", password)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testRegister_Successfully() throws Exception {
+        String email = "user1@email.com";
+        String password = "password";
+        
+        Mockito.when(authenticateService.register(email, password)).thenReturn(authenticateResponse);
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/api/public/authenticate/register")
+                .param("email", email)
+                .param("password", password)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isCreated());
+    }
+}
--- backend/src/test/java/com/fijalkowskim/travelmemories/controllers/UserControllerTest.java
+++ backend/src/test/java/com/fijalkowskim/travelmemories/controllers/UserControllerTest.java
@@ -3,14 +3,10 @@ package com.fijalkowskim.travelmemories.controllers;
 import com.fijalkowskim.travelmemories.exceptions.CustomHTTPException;
 import com.fijalkowskim.travelmemories.services.UserService;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
@@ -19,9 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
