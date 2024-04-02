9f37885b81fe7bd8e04c90c6300b0181bb6014ad
Mateusz <fijalkowskim01@gmail.com>
2024-04-02 16:38:28 +0200

    Authenticate Service authenticate no such user test

--- backend/src/test/java/com/fijalkowskim/travelmemories/services/AuthenticateServiceTest.java
+++ backend/src/test/java/com/fijalkowskim/travelmemories/services/AuthenticateServiceTest.java
@@ -1,5 +1,6 @@
 package com.fijalkowskim.travelmemories.services;
 
+import com.fijalkowskim.travelmemories.exceptions.CustomHTTPException;
 import com.fijalkowskim.travelmemories.models.users.User;
 import com.fijalkowskim.travelmemories.repositories.UserDAORepository;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,5 +40,15 @@ public class AuthenticateServiceTest {
         User authenticatedUser = authenticateService.authenticate(email, password);
 
         assertEquals(authenticatedUser, user);
+    }
+    @Test
+    public void testAuthenticate_NoSuchUser(){
+        String email = "nouser@email.com";
+        when(userDAORepository.findByEmail(email))
+                .thenReturn(Optional.empty());
+
+        assertThrows( CustomHTTPException.class,()->{
+            authenticateService.authenticate(email,"p");
+        });
     }
 }
