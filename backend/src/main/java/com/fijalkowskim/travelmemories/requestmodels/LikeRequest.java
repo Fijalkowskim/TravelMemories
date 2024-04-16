a7b9bdcd31f69c59e8dd478fcabfa2b7b0c06275
Mateusz <fijalkowskim01@gmail.com>
2024-04-16 16:58:38 +0200

    LikeService addLike tests

--- backend/src/main/java/com/fijalkowskim/travelmemories/requestmodels/LikeRequest.java
+++ backend/src/main/java/com/fijalkowskim/travelmemories/requestmodels/LikeRequest.java
@@ -1,8 +1,10 @@
 package com.fijalkowskim.travelmemories.requestmodels;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class LikeRequest {
     private long userId;
     private long photoId;
--- backend/src/test/java/com/fijalkowskim/travelmemories/services/LikeServiceTest.java
+++ backend/src/test/java/com/fijalkowskim/travelmemories/services/LikeServiceTest.java
@@ -6,27 +6,31 @@ import com.fijalkowskim.travelmemories.models.photos.Like;
 import com.fijalkowskim.travelmemories.repositories.LikesDAORepository;
 import com.fijalkowskim.travelmemories.repositories.PhotoDAORepository;
 import com.fijalkowskim.travelmemories.repositories.UserDAORepository;
+import com.fijalkowskim.travelmemories.requestmodels.LikeRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class LikeServiceTest {
     @InjectMocks
+    @Spy
     private LikeService likeService;
     @Mock
     private UserDAORepository userDAORepository;
@@ -80,6 +84,52 @@ public class LikeServiceTest {
         assertThatThrownBy(()->likeService.getLikeById(id))
                 .isInstanceOf(CustomHTTPException.class);
     }
+    @Test
+    public void AddLike_ProperData_Success(){
+        long photoId = 1;
+        long userId = 1;
+        LikeRequest likeRequest = LikeRequest.builder().photoId(photoId).userId(userId).build();
+        Like expectedLike = Like.builder().id(1L).build();
+        when(likesDAORepository.findByPhotoIdAndUserId(photoId,userId)).thenReturn(Optional.empty());
+        when(likesDAORepository.save(expectedLike)).thenReturn(expectedLike);
+        doReturn(expectedLike).when(likeService).likeFromRequest(likeRequest);
+
+        Like createdLike = likeService.addLike(likeRequest);
+
+        assertThat(createdLike).isEqualTo(expectedLike);
+    }
+    @Test
+    public void AddLike_ExistingLike_ExceptionThrown(){
+        long photoId = 2;
+        long userId = 1;
+        LikeRequest likeRequest = LikeRequest.builder().photoId(photoId).userId(userId).build();
+        Like expectedLike = Like.builder().id(1L).build();
+        when(likesDAORepository.findByPhotoIdAndUserId(photoId,userId)).thenReturn(Optional.of(expectedLike));
+
+        assertThatThrownBy(() -> likeService.addLike(likeRequest)).isInstanceOf(CustomHTTPException.class);
+    }
+    @Test
+    public void AddLike_InvalidUserId_ExceptionThrown(){
+        long photoId = 1;
+        long userId = -1;
+        LikeRequest likeRequest = LikeRequest.builder().photoId(photoId).userId(userId).build();
+        when(likesDAORepository.findByPhotoIdAndUserId(photoId,userId)).thenReturn(Optional.empty());
+        doThrow(new CustomHTTPException("No such user", HttpStatus.BAD_REQUEST)).when(likeService).likeFromRequest(likeRequest);
+
+        assertThatThrownBy(() -> likeService.addLike(likeRequest)).isInstanceOf(CustomHTTPException.class);
+    }
+    @Test
+    public void AddLike_InvalidPhotoId_ExceptionThrown(){
+        long photoId = -1;
+        long userId = 1;
+        LikeRequest likeRequest = LikeRequest.builder().photoId(photoId).userId(userId).build();
+        when(likesDAORepository.findByPhotoIdAndUserId(photoId,userId)).thenReturn(Optional.empty());
+        doThrow(new CustomHTTPException("No such photo", HttpStatus.BAD_REQUEST)).when(likeService).likeFromRequest(likeRequest);
+
+        assertThatThrownBy(() -> likeService.addLike(likeRequest)).isInstanceOf(CustomHTTPException.class);
+    }
+
+
     @Test
     public void GetNumberOfLikesForPhoto_ProperId_Success(){
         long id = 1;
